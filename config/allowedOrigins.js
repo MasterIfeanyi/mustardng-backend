2df3f881c374a1466dca225c0b64650be9bfb5d8
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-07-02 16:06:57 +0100

    add gitignore

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+// Cross Origin Resource Sharing
+export const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:3500",
+    'http://127.0.0.1:5500',
+    "https://ifeanyi-mernapp-goals-users.netlify.app"
+]
