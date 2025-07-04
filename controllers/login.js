bb5a2a50de635f82da1aff6848859bf06b633c5f
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-07-04 20:05:21 +0100

    making changes

--- controllers/login.js
+++ controllers/login.js
@@ -5,14 +5,19 @@ const jwt = require('jsonwebtoken');
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
+    
     if (!user) return res.status(401).json({ message: 'Invalid credentials' });
 
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
     res.json({ token });
+
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
