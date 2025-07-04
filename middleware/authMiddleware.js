52805863a5f197b885985c905e9ca55f4a0a9f29
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-07-04 20:50:16 +0100

    add token decode

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
+const User = require("../models/User")
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
+  // const token = req.cookies.accessToken;
 
   if (!token) return res.status(401).json({ message: 'No token, not authorized' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    // req.user = decoded;
+    req.user = await User.findById(decoded.userId);;
     next();
   } catch (err) {
     return res.status(401).json({ message: 'Token invalid or expired' });
