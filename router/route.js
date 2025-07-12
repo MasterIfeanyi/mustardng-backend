aefb2c3397f2373112bb2b58bf619889592a912a
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-07-12 14:30:03 +0100

    add auth routes

--- router/route.js
+++ router/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {register} = require("../controllers/register")
 const {login} = require("../controllers/login")
 const {logout} = require("../controllers/logout")
+const {auth} = require("../controllers/auth")
 const {createProudct, findUserProducts} = require("../controllers/budget")
 const {protect} = require("../middleware/authMiddleware")
 const rateLimit = require("../utils/rate-limit")
@@ -14,6 +15,8 @@ router.post("/login", rateLimit, login);
 router.post("/budget", protect, createProudct);
 
 router.get("/budget", protect, findUserProducts);
+
+router.get("/me", protect, auth);
 
 router.post('/logout', protect, logout)
 
